839789a4816465ab2e79eeddc07c1f626a6e2661
Jussi Kinnunen <jussikin@iki.fi>
2021-01-10 19:11:33 +0200

    added clean routine

--- servitor.py
+++ servitor.py
@@ -1,10 +1,13 @@
+from inky.inky_uc8159 import Inky, CLEAN
+import time
 import paho.mqtt.client as mqtt
 import sys
-
 
 if(len(sys.argv)<3):
     print("first argument mqtt server, second topic")
     quit
+
+inky = Inky()
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -17,9 +20,19 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
+def clean():
+    for _ in range(2):
+     for y in range(inky.height - 1):
+        for x in range(inky.width - 1):
+            inky.set_pixel(x, y, CLEAN)
+
+    inky.show()
+    time.sleep(1.0)
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 print(sys.argv[1])
 client.connect(sys.argv[1], 1883, 60)
+clean()
 client.loop_forever()
