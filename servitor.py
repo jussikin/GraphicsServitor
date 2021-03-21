6b24d557553c11a348fcf74467747701c2924db9
Jussi Kinnunen <jussikin@iki.fi>
2021-03-21 12:40:00 +0200

    added show text feature

--- servitor.py
+++ servitor.py
@@ -2,6 +2,7 @@ from inky.inky_uc8159 import Inky, CLEAN
 import time
 import paho.mqtt.client as mqtt
 import sys
+from PIL import Image, ImageDraw, ImageFont
 
 if(len(sys.argv)<3):
     print("first argument mqtt server, second topic")
@@ -9,9 +10,10 @@ if(len(sys.argv)<3):
 
 inky = Inky()
 
+
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe(sys.argv[2])
@@ -19,20 +21,36 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+    array=msg.payload.split(";",10)
+    cmd=array[0]
+    print(cmd)
+    if(cmd=="cls"):
+	clean()
+    if(cmd=="showbuffer"):
+	show_buffer()
+    if(cmd=="helloworld"):
+        write_text("Hello world!",0,0,(0,0,0,255))
+    if(cmd=="writetext"):
+        write_text(array[1],int(array[2]),int(array[3]),(int(array[4]),int(array[5]),int(array[6]),255))
 
 def clean():
-    for _ in range(2):
-     for y in range(inky.height - 1):
-        for x in range(inky.width - 1):
-            inky.set_pixel(x, y, CLEAN)
-
-    inky.show()
-    time.sleep(1.0)
+    global buffer
+    buffer = Image.new("RGB",(inky.width,inky.height),(255,255,255))
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 print(sys.argv[1])
+buffer = Image.new("RGB",(inky.width,inky.height),(255,255,255))
+font = ImageFont.truetype("whitrabt.ttf", 25)
+
+def show_buffer():
+    inky.set_image(buffer,1)
+    inky.show()
+
+def write_text(text, x, y, color):
+    d = ImageDraw.Draw(buffer)
+    d.text((x, y), text, fill=color,font=font)
+
 client.connect(sys.argv[1], 1883, 60)
-clean()
 client.loop_forever()
